39f6e93471895a3b9aede2dc415af08580311379
yves <webghost779@gmail.com>
2014-10-30 13:28:31 +0100

    Create help.inc.php

---
+++ help.inc.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * seo42 Minifier
+ *
+ * @version 0.1
+ * @author webghost
+ */
+
+
+?>
+<h3>Hilfe</h3>
+
+<ul style="margin-left: 1.2em;">
+      <li><a href="http://forum.redaxo.de">READAXO Forum</a></li>
+      <li><a href="https://github.com/webghostx/minifier">GitHub</a></li>
+</ul>
